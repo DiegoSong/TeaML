e8387cba596bccf79c8fa3b74a3d5564abc954d0
songdian <songdian@didiglobal.com>
2020-03-06 18:58:28 +0800

    .

--- TeaRM/examples.py
+++ TeaRM/examples.py
@@ -38,3 +38,23 @@ tea.cook(encoder)
 tea.select(method)
 tea.drink(LogisticRegression(penalty='l2', C=1, class_weight='balanced'))
 tea.sleep(woe.bins)
+
+
+
+data_matrix = feature_value_info(train_set.drop([i for i in unfeas if i!='is_bad'], axis=1), label_name='is_bad', bin_split=5)
+data_matrix_oot = feature_value_info(oot_set.drop([i for i in unfeas if i!='is_bad'], axis=1), label_name='is_bad', bin_split=5, oot_dm=data_matrix)
+
+feature_iv = {}
+for f in data_matrix_oot:
+    feature_iv[f] = [data_matrix[f]['iv'].sum(), data_matrix_oot[f]['iv'].sum()]
+
+iv_df = pd.DataFrame(feature_iv, index=['iv', 'oot_iv']).T.sort_values('iv', ascending=False)
+
+psi = tag_psi(data_matrix, data_matrix_oot)
+psi_df = pd.DataFrame(psi, index=['psi']).T.sort_values('psi', ascending=False)
+df_info = pd.merge(iv_df, psi_df, how='left', left_index=True, right_index=True)
+df_info.index.name = 'feature'
+df_info = df_info.reset_index()
+
+describe = get_describe(data_new.drop(unfeas, axis=1))
+res = describe.merge(df_info, how='left', left_on=['变量名称'], right_on=['feature'])
